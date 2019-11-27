a9c97ac53472fa99afe35af610d91c4c7e7b033d
yup <yaup22c@naver.com>
2019-11-27 17:22:53 +0900

    week 1 final

--- lec4/lec4_JY.py
+++ lec4/lec4_JY.py
@@ -1,87 +1,201 @@
-# 2869
-import math
+# 4.1 
+def major(matrix):
+    N = len(matrix)
+    majority =-1
+    majority_count =0
+    for index in range(N):
+        element = matrix[index]
+        count =0
+        for index1 in range(N):
+            if matrix[index1]== element:
+                count+=1
+        if count > majority_count:
+            majority = matrix[index]
+            majority_count = count
+            
+    print(majority)
+    print(majority_count)
+            
+mrx = [1,1,2,3,3,3,3,4,4,4]
+major(mrx)
 
-text = input().split(" ")
-numbers = [ int(x) for x in text]
-
-print(math.ceil((numbers[2]-numbers[0])/(numbers[0]-numbers[1])) +1)
-
-# 2798
-line1 = input().split(" ")
-line1 = [int(x) for x in line1]
-
-line2 = input().split(" ")
-line2 = [int(x) for x in line2]
-sota =0
-for i in range(len(line2)):
-    for j in range(i+1, len(line2)):
-        for k in range(j+1,len(line2)):
-            val = line2[i] +line2[j] +line2[k]
-            if val <= line1[1] and line1[1] - val < line1[1]-sota:
-                sota = val
-print(sota)
-
-
-#10250
-count= int(input())
-for _ in range(count):
-  line = input().split(" ")
-  line = [int(x) for x in line]
-  # line[0] = floor , line[1] = range, line[2]=number
-  room =((line[2]-1)//line[0])+1
-  if not (line[2]%line[0]):
-      floor =line[0]
-  else:
-      floor =(line[2]%line[0])   
-  if room <10:
-      name = str(floor)+str(0)+str(room)
-      print(name)
-  else:
-      name = str(floor)+str(room)
-      print(name)
+# 4.2
+def major1(matrix):
+    N = len(matrix)
+    count = [0*x for x in range(101)]
+    for index in range(N):
+        count[matrix[index]]+=1
+    majority = 0
+    for index1 in range(101):
+        if count[index1]> count[majority]:
+            majority = index1
+    print(majority)
+    
+    
+    
+mrx = [1,1,2,3,3,3,3,4,4,4]
+major1(mrx)
 
 
-# 2775
-count= int(input())
-table1 = [x for x in range(1,15)]
-table2 = [int(x*(x+1)/2) for x in range(1,15)]
-table3 = [int(x*(x+1)/4+(x)*(x+1)*(2*x+1)/12) for x in range(1,15)]
+#4.3 Moving average
+def moving_avg(matrix,M):
+    ret = []
+    N = len(matrix)
+    for index in range(M-1,N):
+        mov_sum=0
+        for mov_index in range(M):
+            mov_sum+=matrix[index-mov_index]
+        
+        ret.append(mov_sum/M)
+    print(ret)
+    
+mrx = [1,1,2,3,3,3,3,4,4,4]
+moving_avg(mrx,3)
+    
 
-table = [table1,table2,table3]
-for _ in range(count):
-    K = int(input())
-    N = int(input())
-    if K <=2:
-        print(table[K][N-1])
-    else:
-        while len(table)-1<K:
-            temp=[sum(table[len(table)-1][:x]) for x in range(1,15)]
-            table.append(temp)
-        print(table[K][N-1])
+# 4.4 
+def moving_avg1(matrix,M):
+    ret = []
+    N = len(matrix)
+    mov_sum=0
+    for index in range(0,M-1):
+        mov_sum+=matrix[index]
+    for index1 in range(M-1,N):
+        mov_sum+=matrix[index1]
+        ret.append(mov_sum/M)
+        mov_sum-=matrix[index1-M+1]
+    print(ret)
+    
+mrx = [1,1,2,3,3,3,3,4,4,4]
+moving_avg1(mrx,3)
+    
 
-# 2490
-# 0 배
-# 1 등
-for _ in range(3):
-    line = input().split(" ")
-    line = [int(x) for x in line]
-    out = sum(line)
-    if out ==0:
-        print("D")
-    elif out==1:
-        print("C")
-    elif out==2:
-        print("B")
-    elif out==3:
-        print("A")
-    elif out==4:
-        print("E")
-    else:
-        pass
+# 4.5
+#INF = 987654321
+#menu=[]
+#out_menu = [False for x in range(len(menu))]
+#def menu_finder(menu):
+#    if 
+    
+# 4.6
+def factor(N):
+    if N==1:
+        print("no prime")
+    output= []
+    for div in range(2,N):
+        while(N%div ==0):
+            N = N/div
+            output.append(div)
+    print(output)
 
-# 2163
+N= 15
+factor(N)
+    
+    
+# 4.7
+def firstindex(matrix,element):
+    for index in range(len(matrix)):
+        if matrix[index]==element:
+            return index
+    return -1
 
-line = input().split(" ")
-N = int(line[0])
-M = int(line[1])
-print(N*M-1)
+mrx = [1,2,3,4,5]
+print(firstindex(mrx,7))
+
+
+
+# 4.8 s
+
+def selection_srt(matrix):
+    for index in range(len(matrix)):
+        min_index=index
+        for index1 in range(index+1,len(matrix)):
+            if matrix[min_index]> matrix[index1]:
+                min_index=index1
+        matrix[index],matrix[min_index] = matrix[index],matrix[min_index]
+        
+    return matrix
+        
+        
+def insertion_srt(matrix):
+    for index in range(len(matrix)):
+        j = index
+        while(j>0 and matrix[j-1] > matrix[j]):
+            matrix[j],matrix[j-1] = matrix[j-1],matrix[j]
+            j-=1
+    return matrix
+marx =[5,4,3,2,1]
+
+print(insertion_srt(marx))
+print(selection_srt(marx))
+        
+
+    
+# 4.9
+def inefficientmaxsum(matrix):
+    N = len(matrix)
+    ret =matrix[0]
+    for i in range(N):
+        for j in range(i+1,N):
+            part_sum=0
+            for k in range(i,j):
+                part_sum+=matrix[k]
+            ret = max(ret,part_sum)
+    print(ret)
+    
+marx= [-1,-2,-3,-4,-5]
+
+inefficientmaxsum(marx)
+
+# 4.10
+def bettermaxsum(matrix):
+    N = len(matrix)
+    ret=matrix[0]
+    for i in range(N):
+        part_sum=0
+        for j in range(i,N):
+            part_sum+=matrix[j]
+            ret = max(ret,part_sum)
+    print(ret)
+
+bettermaxsum(marx)    
+
+# 4.11
+def fastmaxsum(matrix,lo,hi):
+    if lo==hi:
+        return matrix[lo]
+    
+    part_sum=0
+    mid = int((lo+hi)/2)
+    left = matrix[lo]
+    right= matrix[hi]
+    for i in range(lo,mid,-1):
+        part_sum+=matrix[i]
+        left = max(left,part_sum)
+    part_sum=0
+    for j in range(mid+1,hi):
+        part_sum+=matrix[j]
+        right = max(right,part_sum)
+        
+    single = max(fastmaxsum(matrix,lo,mid),fastmaxsum(matrix,mid+1,hi))
+    return max(left+right,single)
+
+print(fastmaxsum(marx,0,4))
+
+
+#4.12
+def fastestmaxsum(matrix):
+    N = len(matrix)
+    ret=matrix[0]
+    psum=0
+    for i in range(N):
+        psum=max(psum,0)+matrix[i]
+        ret = max(psum,ret)
+    print(ret)
+    
+    
+fastestmaxsum(marx)
+    
+    
+    
+    
