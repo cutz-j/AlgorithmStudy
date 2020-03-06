d431085735a4359d9883c9111fa52a4dda17e435
cutz-j <cutz-j@naver.com>
2020-03-06 11:02:21 +0900

    Update [cutz]packing.py

--- lec9/[cutz]packing.py
+++ lec9/[cutz]packing.py
@@ -1,59 +1,45 @@
 # time over
 import sys
 
-volume_list, need_list, name_list = [], [], []
+volume_list = []
+need_list = []
+name_list = []
 n = -1 # item length wanted
-cache = [[]] # dp
+cache = [] # dp
 picked = []
 
 def pack(capacity, i):
-    '''
-    function: calculate mximum needs in the capacity
-    input:
-        - capacity
-        - i
-    output:
-        - maximum needs
-    '''
+
     # basis
-    if i == n:
+    if cache[capacity][i] != -1:
+        return cache[capacity][i]
+    if i >= len(name_list) or capacity <= 0:
         return 0
-    ret = cache[capacity][i]
-    if ret != -1:
-        return ret
     # not pick up
-    ret = pack(capacity, i+1)
-    
-    # pick up
-    if capacity >= volume_list[i]:
-        ret = max(ret, pack(capacity - volume_list[i], i+1) + need_list[i])
-    cache[capacity][i] = ret
-    return ret
+    result = pack(capacity, i+1)
+    if volume_list[i] <= capacity:
+        result = max(result, need_list[i] + pack(capacity-volume_list[i], i+1))
+    cache[capacity][i] = result
+    return result
 
 def reconstruct(capacity, i):
-    '''
-    function:
-        
-    '''
     # basis
-    if i == n:
+    if i >= len(name_list):
         return
-    # present item != next item --> present maximum list
-    present_n = pack(capacity, i)
-    next_n = pack(capacity, i+1)
-    if present_n == next_n:
+
+    if pack(capacity, i) == pack(capacity, i+1):
         reconstruct(capacity, i+1)
     else:
         picked.append(name_list[i])
-        reconstruct(capacity - volume_list[i], i+1)
+        reconstruct(capacity-volume_list[i], i+1)
 
     
 
-rl = lambda : sys.stdin.readline()
-#rl = input
+#rl = lambda : sys.stdin.readline()
+rl = input
 for _ in range(int(rl())):
     n, w = map(int, rl().split())
-    cache = [[-1] * (n + 1) for _ in range(w + 1)]
+    cache = [[-1]*(n+1) for _ in range(w+1)]
         
     for i_ in range(n):
         item, volume, need = rl().split()
