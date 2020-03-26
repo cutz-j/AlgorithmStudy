08c10ddd01b48fc94a124bba5bbdb3953e194983
cutz-j <cutz-j@naver.com>
2020-03-26 20:28:11 +0900

    Update

--- lec10/[cutz]lunchbox.py
+++ lec10/[cutz]lunchbox.py
@@ -1,2 +1,26 @@
 import sys
 
+
+rl = lambda : sys.stdin.readline()
+#rl = input
+
+C = int(rl())
+
+for _ in range(C):
+    N = int(rl())
+    pair = {}
+    
+    M = list(map(int, rl().split()))
+    E = list(map(int, rl().split()))
+    
+    for i, e in enumerate(E):
+        pair[i] = e
+        
+    pair_sort = sorted(pair.items(), key = lambda item: item[1], reverse=True) # value: [1]
+    ret, eat = 0, 0
+    for i in range(N):
+        box = pair_sort[i][0]
+        eat += M[box]
+        ret = max(ret, eat + E[box])
+        
+    print(ret)
