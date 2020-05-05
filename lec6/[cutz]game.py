6a69e310decd9218a4aaec7b1d98053cb9dba614
Hyeonseong Jeon <cutz-j@naver.com>
2020-05-05 16:34:20 +0900

    Update [cutz]game.py

--- lec6/[cutz]game.py
+++ lec6/[cutz]game.py
@@ -1,6 +1,9 @@
 import sys
+sys.setrecursionlimit(100000)
 
-def game(y, x, cnt): # 완전탐색 --> 재귀초
+def game(y, x, cnt): # 완전탐색 --> 재귀초과(무한번 케이스)
+    if cnt > 100:
+        return -1
     if y < 0 or y >= N:
         return cnt
     elif x < 0 or x >= M:
@@ -15,9 +18,34 @@ def game(y, x, cnt): # 완전탐색 --> 재귀초
         top = game(y-X, x, cnt+1) # top
         return max([right, bottom, left, top])
     
+def game_dp(y, x):
+    if y >= N or y < 0 or x >= M or x < 0 or state[y][x] == 'H':
+        return 0
+    if visited[y][x]:
+        print(-1)
+        exit(0)
+        
+    if cache.get((y, x), 0) != 0:
+        return cache[(y, x)]
+    
+    visited[y][x] = True
+    for i in range(4):
+        value = int(state[y][x])
+        ny = y + value * dy[i]
+        nx = x + value * dx[i]
+        cache[(y, x)] = max(cache.get((y, x), 0), game_dp(ny,nx)+1)
+    visited[y][x] = False
+    
+    return cache[(y, x)]
+    
+    
+    
+    
+    
 #rl = lambda: sys.stdin.readline()
 rl = input
 
+dy, dx = [-1, 0, 1, 0], [0, 1, 0, -1]
 N, M = map(int, rl().split())
 state = []
 for _ in range(N):
@@ -26,4 +54,6 @@ for _ in range(N):
     for letter in row:
         row_list.append(letter)
     state.append(row_list)
-print(game(0, 0, 0))
+cache = {}
+visited = [[False]*M for _ in range(N)]
+print(game_dp(0, 0))
