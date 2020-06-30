99d47fdb0c5ba316e4ddba4bea8cbffcf8a5fca4
Hyeonseong Jeon <cutz-j@naver.com>
2020-06-30 11:31:25 +0900

    Update

--- lec8/[cutz]stair.py
+++ lec8/[cutz]stair.py
@@ -1,14 +1,27 @@
 import sys
-
-def dp(now, step):
-    if now == len_stair-1:
-        return stair[now]
     
-    
-
 
 #rl = lambda: sys.stdin.readline()
 rl = input
 
 len_stair = int(rl())
 stair = [int(rl()) for _ in range(len_stair)]
+dp = [0 for _ in range(len(stair))]
+
+if len(stair) == 1:
+    dp[0] = stair[0]
+
+elif len(stair) == 2:
+    dp[0] = stair[0]
+    dp[1] = max(stair[0]+stair[1], stair[1])
+
+else:
+    dp[0] = stair[0]
+    dp[1] = max(stair[0]+stair[1], stair[1])
+    dp[2] = max(stair[0]+stair[2], stair[1]+stair[2])
+    
+    for i in range(3, len(stair)):
+        dp[i] = max(dp[i-2] + stair[i], dp[i-3] + stair[i-1] + stair[i])
+    
+
+print(dp[-1])
---
+++ lec8/[cutz]triangle.py
@@ -0,0 +1,12 @@
+import sys
+
+
+#rl = lambda sys.stdin.readline()
+rl = input
+
+n = int(rl())
+traiangle = []
+for _ in range(n):
+    traiangle.append(list(map(int, rl().split())))
+    
+    
