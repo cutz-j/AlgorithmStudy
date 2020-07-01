f61e015701a6b2847295747c492ab23d149840eb
Hyeonseong Jeon <cutz-j@naver.com>
2020-07-01 11:20:57 +0900

    Update [cutz]triangle.py

--- lec8/[cutz]triangle.py
+++ lec8/[cutz]triangle.py
@@ -1,12 +1,25 @@
 import sys
 
+def dp(now, prev_idx):
+    if now == n-1:
+        return triangle[now][prev_idx]
+    
+    if cache.get((now, prev_idx), -1) != -1:
+        return cache[(now, prev_idx)]
+    
+    dp_left = dp(now+1, prev_idx)
+    dp_right = dp(now+1, prev_idx+1)
+    cache[(now, prev_idx)] = triangle[now][prev_idx] + max(dp_left, dp_right)
+    return cache[(now, prev_idx)]
+     
 
-#rl = lambda sys.stdin.readline()
+#rl = lambda: sys.stdin.readline()
 rl = input
 
 n = int(rl())
-traiangle = []
+triangle = []
+cache = {}
 for _ in range(n):
-    traiangle.append(list(map(int, rl().split())))
+    triangle.append(list(map(int, rl().split())))
     
-    
+print(dp(0, 0))
