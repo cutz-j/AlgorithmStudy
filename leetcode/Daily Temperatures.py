5f02680620cea0e8a2be5d232593b037355541d7
yupjun <yaup22c@naver.com>
2020-07-07 22:48:14 +1000

    Update Daily Temperatures.py
    JY 07 07

--- leetcode/Daily Temperatures.py
+++ leetcode/Daily Temperatures.py
@@ -24,3 +24,16 @@ class Solution:
                 elif inner_index == len(T[index+1:])-1:
                     answer.append(0)
         return answer
+   
+
+class Solution(object):
+    def dailyTemperatures(self, T):
+        ans = [0] * len(T)
+        stack = [] #indexes from hottest to coldest
+        for i in xrange(len(T) - 1, -1, -1):
+            while stack and T[i] >= T[stack[-1]]:
+                stack.pop()
+            if stack:
+                ans[i] = stack[-1] - i
+            stack.append(i)
+        return ans
