718caaf7c0844edec99a30b3c943ceb8f89df901
Hyeonseong Jeon <cutz-j@naver.com>
2020-07-28 11:44:38 +0900

    Update [cutz]elec.py

--- lec31_spanningtree/[cutz]elec.py
+++ lec31_spanningtree/[cutz]elec.py
@@ -12,40 +12,41 @@ def find(x):
     return x
 
 def merge(a, b):
-    parent[a] = b
+    parent[b] = a
 
 
 rl = input
 # rl = lambda: sys.stdin.readline()
-
-m, n = map(int, rl().split())
-adj = []
-ans = 0
-for _ in range(n):
-    x, y, z = map(int, rl().split())
-    adj.append((z, (x, y)))
-    ans += z
-
+while True:
+    m, n = map(int, rl().split())
+    if m == n == 0:
+        break
+    adj = []
+    ans = 0
+    for _ in range(n):
+        x, y, z = map(int, rl().split())
+        adj.append((z, (x, y)))
+        ans += z
     
-adj.sort()
-
-parent = {i:i for i in range(m)}
-
-cnt, sum_cost = 0, 0
-for i in range(n):
-    cost = adj[i][0]
-    x = adj[i][1][0]
-    y = adj[i][1][1]
+        
+    adj.sort()
     
-    x, y = find(x), find(y)
-    if x == y:
-        continue
+    parent = {i:i for i in range(m)}
     
-    cnt += 1
-    sum_cost += cost
-    merge(x, y)
-
-ans -= sum_cost
-print(ans)
-end_a, end_b = map(int, rl().split())
+    cnt, sum_cost = 0, 0
+    for i in range(n):
+        cost = adj[i][0]
+        x = adj[i][1][0]
+        y = adj[i][1][1]
+        
+        x, y = find(x), find(y)
+        if x == y:
+            continue
+        
+        cnt += 1
+        sum_cost += cost
+        merge(x, y)
+    
+    ans -= sum_cost
+    print(ans)
 
