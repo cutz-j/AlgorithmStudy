3c5e489b10f2b98ed740af5370676ee3a8c3b050
Hyeonseong Jeon <cutz-j@naver.com>
2020-08-20 17:13:26 +0900

    Update [cutz]cave.py

--- kakao/[cutz]cave.py
+++ kakao/[cutz]cave.py
@@ -1,17 +1,15 @@
 result = []
 
-def dfs(here, visited, adj, dependency, bool_graph):
-    visited[here] += 1
-    if bool_graph.get(here, True) == False:
-        return
-    if dependency.get(here, None):
-        bool_graph[dependency[here]] = True
-    
-    for there in adj[here]:
-        dfs(there, visited, adj, dependency, bool_graph)
+def dfs(here, visited, adj):
+    visited[here] += 1    
+    for there in adj.get(here, []):
+        if visited[there] == 0:
+            dfs(there, visited, adj)
     result.append(here)
 
 def solution(n, path, order):
+    global result
+    
     answer = True
     adj = {}
     for a, b in path:
@@ -19,16 +17,25 @@ def solution(n, path, order):
             adj[a].append(b)
         else:
             adj[a] = [b]
-        if adj.get(b, []):
-            adj[b].append(a)
+    for a, b in order:
+        if adj.get(a, []):
+            adj[a].append(b)
         else:
-            adj[b] = [a]
-    dependency = {a:b for a, b in order}
-    bool_graph = {b:False for a, b in order}
+            adj[a] = [b]
+            
     
     visited = [0] * n
     for i in range(n):
-        dfs(i, visited, adj, dependency, bool_graph)
+        if visited[i] == 0:
+            dfs(i, visited, adj)
     
-            
+    result = result[::-1]
+    for i in range(n):
+        for j in range(i+1, n):
+            if adj.get(result[j], []):
+                if result[i] in adj[result[j]]:
+                    return False
+
     return answer
+
+# binary search
