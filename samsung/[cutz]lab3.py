90715f7fd9dbcec6e32fb0d4688fb5db57cd97ac
cutz-j <cutz-j@naver.com>
2020-09-01 11:29:56 +0900

    Update [cutz]lab3.py

--- samsung/[cutz]lab3.py
+++ samsung/[cutz]lab3.py
@@ -4,27 +4,18 @@ import copy
 
 def step(virus_map, start, cnt):
     res = copy.deepcopy(virus_map)
-    virus_num = 0
-    for i in range(N):
-        for j in range(N):
-            if lab[i][j] == 1:
-                continue
-            elif lab[i][j] == 2:
-                if (i, j) not in start:
-                    continue
-        
-            value = virus_map[i][j]
-            if value == 1:
-                for k in range(4):
-                    new_row, new_col = i+row_dir[k], j+col_dir[k]
-                    if (0 <= new_row < N) and (0 <= new_col < N):
-                        if res[new_row][new_col] == 0:
-                            if lab[new_row][new_col] != 1 and lab[new_row][new_col] != 2:
-                                res[new_row][new_col] = 1
-                                virus_num += 1
-        
-    return res, cnt+1, virus_num
-
+    virus_num, virus_list = 0, []
+    
+    for row, col in start:
+        for k in range(4):
+            new_row, new_col = row+row_dir[k], col+col_dir[k]
+            if (0 <= new_row < N) and (0 <= new_col < N):
+                if res[new_row][new_col] == 0:
+                    if lab[new_row][new_col] != 1 and lab[new_row][new_col] != 2:
+                        res[new_row][new_col] = 1
+                        virus_num += 1
+                        virus_list.append((new_row, new_col))
+    return res, cnt+1, virus_num, virus_list
 
 
 rl = input
@@ -46,15 +37,18 @@ for i in range(N):
 space += M
 
 ans = sys.maxsize
-for comb in combinations(virus, M):
+comb_set = combinations(virus, M)
+for comb in comb_set:
+    start = []
     virus_num, cnt = 0, 0
     virus_map = [[0]*N for _ in range(N)]
     for row, col in comb:
         virus_map[row][col] = 1
         virus_num += 1
+        start.append((row, col))
     
     while True:
-        virus_map, cnt, v_num = step(virus_map, comb, cnt)
+        virus_map, cnt, v_num, start = step(virus_map, start, cnt)
         virus_num += v_num
         if virus_num == space:
             if cnt < ans:
