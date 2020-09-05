42885908f38c04f95ff5e19586a581bbc93392d9
cutz-j <cutz-j@naver.com>
2020-09-05 15:51:46 +0900

    Update

---
+++ samsung/[cutz]2048.py
@@ -0,0 +1,14 @@
+import sys
+
+def step(board, direction):
+    a
+
+
+rl = input
+# rl = lambda: sys.stdin.readline()
+
+N = int(rl())
+board = []
+for _ in range(N):
+    board.append(list(map(int, rl().split())))
+    
