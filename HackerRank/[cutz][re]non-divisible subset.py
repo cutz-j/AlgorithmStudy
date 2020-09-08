20dc998e32d40febe3bd117a293792c5fc46078f
Hyeonseong Jeon <cutz-j@naver.com>
2020-09-08 23:19:24 +0900

    Update [cutz][re]non-divisible subset.py

--- HackerRank/[cutz][re]non-divisible subset.py
+++ HackerRank/[cutz][re]non-divisible subset.py
@@ -11,7 +11,7 @@ def nonDivisibleSubset(k, s):
             remainder[rem] = 1
         
     for i in range(1, k//2+1):
-        if i != k-1:
+        if i != k-i:
             # i를 나머지로 갖는 수와 k-1를 나머지로 갖는 수는 그 중 max
             result += max(remainder.get(i, 0), remainder.get(k-i, 0))
         else:
