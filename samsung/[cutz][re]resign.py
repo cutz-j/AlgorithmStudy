4803288988161c0cfd8cd642c7cdb6eb7f7ac762
cutz-j <cutz-j@naver.com>
2020-09-15 11:27:11 +0900

    Update

---
+++ samsung/[cutz][re]resign.py
@@ -0,0 +1,32 @@
+import sys
+
+def step(t, r):
+    if cache.get(t, -1) != -1:
+        return cache[t]
+
+    if t >= N:
+        return r
+
+    for j in range(t, N):
+        if j+time[j] <= N:
+            cache[t] = max(step(j+time[j], r+reward[j]), cache.get(t, 0))
+        else:
+            cache[t] = r
+    return r
+
+
+rl = lambda: sys.stdin.readline()
+
+N = int(rl())
+time, reward = [], []
+for _ in range(N):
+    t, p = map(int, rl().split())
+    time.append(t)
+    reward.append(p)
+# visited = [-1 for _ in range(N)]
+cache = {}
+step(0, 0)
+if cache:
+    print(max(cache.values()))
+else:
+    print(0)
