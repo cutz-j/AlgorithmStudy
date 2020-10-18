c18c71e6f96502e45c69a52b762864bf2a9a5169
cutz-j <cutz-j@naver.com>
2020-10-18 19:39:00 +0900

    Update

--- leetcode/[cutz][re]trapping.py
+++ leetcode/[cutz][re]trapping.py
@@ -1,1 +1,38 @@
-z
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        volume = 0
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+
+        # 순회
+        while left < right:
+            left_max, right_max = max(left_max, height[left]), max(right_max, height[right])
+
+            if left_max <= right_max:
+                volume += left_max - height[left]
+                left += 1
+            else:
+                volume += right_max - height[right]
+                right -= 1
+        return volume
+
+    def trap_stack(self, height: List[int]) -> int:
+        stack = []
+        volume = 0
+
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+
+                if not len(stack):
+                    break
+
+                # 이전과의 차이
+                dist = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+                volume += dist * waters
+            stack.append(i)
+        return volume
