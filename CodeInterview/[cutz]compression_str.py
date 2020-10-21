14c124138a3d9659fbb1967a285000ea9ae1919c
cutz-j <cutz-j@naver.com>
2020-10-21 20:26:31 +0900

    Update [cutz]compression_str.py

--- CodeInterview/[cutz]compression_str.py
+++ CodeInterview/[cutz]compression_str.py
@@ -1,3 +1,10 @@
 from collections import defaultdict
 
-def solve()
+def solve(s):
+    hash_dict = defaultdict(int)
+
+    for i in range(len(s)):
+        hash_dict[s[i]] += 1
+        
+
+    return
